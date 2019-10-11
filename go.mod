3ab576603799c491f189ca5e2e5bb764a4b7918c
Ilija Matoski <ilijamt@gmail.com>
2019-10-11 21:38:36 +0200

    Added tests for tree functionality

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/gammazero/workerpool v0.0.0-20191005194639-971bc780f6d7
+	github.com/google/uuid v1.1.1
 	github.com/hashicorp/vault/api v1.0.4
 	github.com/hashicorp/vault/sdk v0.1.13
 	github.com/ilijamt/envwrap v0.0.0-20180905194559-4f93509209b0
--- internal/vault/tree_test.go
+++ internal/vault/tree_test.go
@@ -1,9 +1,13 @@
 package vault_test
 
 import (
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/ilijamt/vht/internal/vault"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"testing"
+	"time"
 )
 
 func TestTree(t *testing.T) {
@@ -14,6 +18,33 @@ func TestTree(t *testing.T) {
 	})
 
 	t.Run("Listing a path", func(t *testing.T) {
+		client, err := vault.Client()
+		require.NoError(t, err)
+		require.NotNil(t, client)
+		type Data struct {
+			Test string
+			Time time.Time
+		}
+		uroot := uuid.New()
+		for i := 0; i < 10; i++ {
+			uuid1 := uuid.New()
+			uuid2 := uuid.New()
+			uuid3 := uuid.New()
+			require.NoError(t, err)
+			_, err = client.Logical().Write(fmt.Sprintf("secret/data/%s/%s/%s", uroot.String(), "root", uuid1.String()), map[string]interface{}{"data": Data{
+				Test: uuid1.String(),
+				Time: time.Now(),
+			}})
+			_, err = client.Logical().Write(fmt.Sprintf("secret/data/%s/root/%s/%s", uroot.String(), uuid2.String(), uuid3.String()), map[string]interface{}{"data": Data{
+				Test: uuid3.String(),
+				Time: time.Now(),
+			}})
+		}
 
+		paths, err := vault.Tree(fmt.Sprintf("secret/metadata/%s", uroot.String()), client)
+		require.NoError(t, err)
+		require.NotEmpty(t, paths)
+		require.Len(t, paths, 31)
+		require.NoError(t, vault.DeletePaths(paths, client, ioutil.Discard))
 	})
 }
